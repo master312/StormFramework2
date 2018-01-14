d02b39abbba5c2d0066c17fb254cba8e4bdd3b5d
Petar Ostojic <wowhellmail@gmail.com>
2018-01-14 19:27:12 +0100

    Destroy object command now accepts handle or object id as argument.

--- bin/data/lua_common/extend_scene.lua
+++ bin/data/lua_common/extend_scene.lua
@@ -4,6 +4,12 @@ function Scene:instantiatePrefab(prefabName, newObjectName)
     return getObjectHandle(cppObject.id)
 end
 
-function Scene:destroyObject(handle)
-    self:intDestroyObject(handle.id)
+function Scene:destroyObject(handleOrId)
+    if type(handleOrId) == "number" then
+        self:intDestroyObject(handleOrId)
+    elseif handleOrId.id ~= nil then
+        self:intDestroyObject(handleOrId.id)
+    else
+        debug.log("Scene:destroyObject invalid argument");
+    end
 end
